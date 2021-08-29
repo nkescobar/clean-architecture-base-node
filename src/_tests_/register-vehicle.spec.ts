7eba364ecdef9e42b9c4332e0cb4077f3e8175c9
nescobar <nescobar@cotrafa.local>
2021-08-29 14:19:41 -0500

    refactor: add code un register controller

--- src/_tests_/register-vehicle.spec.ts
+++ src/_tests_/register-vehicle.spec.ts
@@ -1,11 +1,11 @@
 import { RegisterVehicle } from '../controllers'
 
-describe('RegisterVehicle', () => {
+describe('Controller RegisterVehicle', () => {
   test('is the name does not exist return 400', () => {
     const sut = new RegisterVehicle()
     const httpRequest = {
       body: {
-      //  name: 'Nissan',
+        //  name: 'Nissan',
         model: 'Dxt',
         year: 2020
       }
@@ -27,5 +27,19 @@ describe('RegisterVehicle', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('error in the model'))
+  })
+
+  test('is the year does not exist return 400', () => {
+    const sut = new RegisterVehicle()
+    const httpRequest = {
+      body: {
+        name: 'Nissan',
+        model: 'Dxt'
+        // year: 2020
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('error in the year'))
   })
 })
--- src/controllers/vehicle/register-vehicle.controller.ts
+++ src/controllers/vehicle/register-vehicle.controller.ts
@@ -1,16 +1,13 @@
 import { HttpRequestInterface, HttpResponseInterface } from '../../interfaces'
 export class RegisterVehicle {
   handle (httpRequest: HttpRequestInterface): HttpResponseInterface {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('error in the name')
-      }
-    }
-    if (!httpRequest.body.model) {
-      return {
-        statusCode: 400,
-        body: new Error('error in the model')
+    const requiredProperties = ['name', 'model', 'year']
+    for (const props of requiredProperties) {
+      if (!httpRequest.body[props]) {
+        return {
+          statusCode: 400,
+          body: new Error(`error in the ${props}`)
+        }
       }
     }
 
